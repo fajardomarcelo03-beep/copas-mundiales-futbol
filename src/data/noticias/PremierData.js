6c1302e8f639704b21f97adc5f035975722fbdbc
fajardomarcelo03-beep <afdm721307@gmail.com>
2026-06-04 10:04:13 -0500

    Agregar notica 1 premier league

--- src/data/noticias/PremierData.js
+++ src/data/noticias/PremierData.js
@@ -1,11 +1,23 @@
 export const noticiasPremier = [
-  {
-    id: "premier-1",
-    categoria: "premier-league",
-    logo: "/logos/premier.png",
-    fechaISO: "2026-06-03",
-    imagen: "/ruta-imagen.jpg",
-    es: { titulo: "", subtitulo: "", contenido: "", fecha: "", tiempoLectura: "" },
-    en: { titulo: "", subtitulo: "", contenido: "", fecha: "", tiempoLectura: "" }
+{
+  id: "noticia-11",
+  categoria: "premier-league",
+  logo: "/logo/premier.png",
+  fechaISO: "2026-06-04",
+  imagen: "/Premier/Iraola.jpg",
+  es: {
+    titulo: "El vasco Andoni Iraola es el nuevo DT del Liverpool",
+    subtitulo: "El técnico español llega al banquillo de los 'Reds' tras alcanzar un acuerdo por dos temporadas.",
+    contenido: "El DT vasco será el sustituto de Arne Slot en el banquillo de los 'Reds' tras llegar a un acuerdo con la directiva por dos años, según informaron medios ingleses. La medida fue tomada a raíz de una campaña decepcionante en la anterior temporada de la Premier League.",
+    fecha: "4 de junio de 2026",
+    tiempoLectura: "2 min"
+  },
+  en: {
+    titulo: "Basque manager Andoni Iraola is the new Liverpool head coach",
+    subtitulo: "The Spanish manager joins the 'Reds' dugout after reaching an agreement for two seasons.",
+    contenido: "The Basque manager will replace Arne Slot at the 'Reds' helm after reaching a two-year agreement with the board, according to English media. The decision was made following a disappointing campaign in the previous Premier League season.",
+    fecha: "June 4, 2026",
+    tiempoLectura: "2 min"
   }
+}
 ];
