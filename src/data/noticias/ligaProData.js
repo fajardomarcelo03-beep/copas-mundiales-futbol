7d85b12a42b880ae84b2635064c1b2b8506cad1e
fajardomarcelo03-beep <afdm721307@gmail.com>
2026-06-04 17:38:00 -0500

    Actualizacion noticias ligapro

--- src/data/noticias/ligaProData.js
+++ src/data/noticias/ligaProData.js
@@ -19,5 +19,47 @@ export const noticiasLigaPro = [
     fecha: "June 4, 2026",
     tiempoLectura: "2 min"
   }
+},
+{
+  id: "noticia-2",
+  categoria: "ligapro",
+  logo: "/logo/ligapro.png",
+  fechaISO: "2026-06-04",
+  imagen: "/LigaPro/Oyola.jpg",
+  es: {
+    titulo: "Matías Oyola deja Barcelona",
+    subtitulo: "El 'Poni' se aparta de su cargo tras la eliminación en la Copa Libertadores.",
+    contenido: "Matías Oyola dejó el cargo de Director Deportivo en Barcelona SC por supuestas discrepancias con la directiva y el DT César Farías a causa de la eliminación en la Copa Libertadores; aún se espera el pronunciamiento oficial del club.",
+    fecha: "4 de junio de 2026",
+    tiempoLectura: "2 min"
+  },
+  en: {
+    titulo: "Matías Oyola leaves Barcelona",
+    subtitulo: "The 'Poni' steps down from his position following the elimination from the Copa Libertadores.",
+    contenido: "Matías Oyola has left his role as Sporting Director at Barcelona SC due to alleged disagreements with the board and manager César Farías following their elimination from the Copa Libertadores; the club's official statement is still pending.",
+    fecha: "June 4, 2026",
+    tiempoLectura: "2 min"
+  }
+},
+{
+  id: "noticia-3",
+  categoria: "ligapro",
+  logo: "/logo/ligapro.png",
+  fechaISO: "2026-06-04",
+  imagen: "/LigaPro/Velez.jpg",
+  es: {
+    titulo: "Paúl Vélez deja de ser DT de Mushuc Runa",
+    subtitulo: "El estratega fue removido de su cargo tras la eliminación en la Copa Ecuador.",
+    contenido: "El técnico ecuatoriano Paúl Vélez fue removido del cargo por decisión de la directiva del Club Mushuc Runa, encabezada por Luis Alfonso Chango, tras una decepcionante presentación ante Gualaceo por la Copa Ecuador, donde quedaron eliminados. Además, se espera que en las próximas horas se anuncie la salida de varios jugadores y la llegada de nuevas contrataciones.",
+    fecha: "4 de junio de 2026",
+    tiempoLectura: "2 min"
+  },
+  en: {
+    titulo: "Paúl Vélez is no longer the head coach of Mushuc Runa",
+    subtitulo: "The manager was removed from his position following their elimination from the Copa Ecuador.",
+    contenido: "Ecuadorian coach Paúl Vélez has been removed from his position by the board of Club Mushuc Runa, led by Luis Alfonso Chango, following a disappointing performance against Gualaceo in the Copa Ecuador, which resulted in their elimination. Furthermore, the departure of several players and the announcement of new signings are expected to be confirmed in the coming hours.",
+    fecha: "June 4, 2026",
+    tiempoLectura: "2 min"
+  }
 }
 ];
