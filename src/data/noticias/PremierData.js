1f84a49fb6e733fb8fa9f8b45bbd4b1a52304e00
fajardomarcelo03-beep <afdm721307@gmail.com>
2026-06-04 18:20:46 -0500

    Agregar notica-2 premier league

--- src/data/noticias/PremierData.js
+++ src/data/noticias/PremierData.js
@@ -1,6 +1,27 @@
 export const noticiasPremier = [
 {
-  id: "noticia-11",
+  id: "noticia-2",
+  categoria: "premier-league",
+  logo: "/logo/premier.png",
+  fechaISO: "2026-06-04",
+  imagen: "/Premier/Community.jpg",
+  es: {
+    titulo: "Arsenal vs Manchester City por la corona de la Community Shield",
+    subtitulo: "El tradicional duelo marcará el inicio de la temporada 2026-2027.",
+    contenido: "El partido se disputará en Gales el domingo 16 de agosto. Este duelo marcará el inicio de una nueva temporada para los dos clubes, quienes vuelven a verse las caras con el primer título en juego del año futbolístico en Inglaterra.",
+    fecha: "4 de junio de 2026",
+    tiempoLectura: "2 min"
+  },
+  en: {
+    titulo: "Arsenal vs Manchester City for the Community Shield crown",
+    subtitulo: "The traditional clash will mark the start of the 2026-2027 season.",
+    contenido: "The match will be played in Wales on Sunday, August 16th. This duel will mark the beginning of a new season for both clubs, as they face each other once again with the first title of the English football year at stake.",
+    fecha: "June 4, 2026",
+    tiempoLectura: "2 min"
+  }
+},
+{
+  id: "noticia-1",
   categoria: "premier-league",
   logo: "/logo/premier.png",
   fechaISO: "2026-06-04",
