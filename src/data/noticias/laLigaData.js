dcaef092e101be313976e9c0d45aa56983650d4c
fajardomarcelo03-beep <afdm721307@gmail.com>
2026-06-04 22:05:28 -0500

    Ingresar noticia-1 la liga

--- src/data/noticias/laLigaData.js
+++ src/data/noticias/laLigaData.js
@@ -1,11 +1,23 @@
 export const noticiasLaLiga = [
-  {
-    id: "liga-1",
-    categoria: "la-liga",
-    logo: "/logos/la-liga.png",
-    fechaISO: "2026-06-03",
-    imagen: "/ruta-imagen.jpg",
-    es: { titulo: "", subtitulo: "", contenido: "", fecha: "", tiempoLectura: "" },
-    en: { titulo: "", subtitulo: "", contenido: "", fecha: "", tiempoLectura: "" }
+ {
+  id: "liga-1",
+  categoria: "la-liga",
+  logo: "/logos/la-liga.png",
+  fechaISO: "2026-06-04",
+  imagen: "/LaLiga/Dumfries.jpg",
+  es: {
+    titulo: "Denzel Dumfries, posible fichaje del Real Madrid",
+    subtitulo: "Florentino Pérez busca reforzar la banda derecha con el lateral neerlandés del Inter de Milán.",
+    contenido: "Florentino Pérez, actual presidente del Real Madrid, anunció que de ganar las elecciones para un nuevo periodo previstas para este domingo, confirmará el fichaje del jugador neerlandés Denzel Dumfries, actualmente en el Inter de Milán. El jugador de 1,88 metros de altura se caracteriza por su buen juego aéreo, velocidad en la marca y desbordes por la banda derecha. El club merengue está dispuesto a abonar los 20 millones de euros de su cláusula de rescisión.",
+    fecha: "4 de junio de 2026",
+    tiempoLectura: "2 min"
+  },
+  en: {
+    titulo: "Denzel Dumfries, a potential signing for Real Madrid",
+    subtitulo: "Florentino Pérez looks to strengthen the right flank with the Dutch fullback from Inter Milan.",
+    contenido: "Florentino Pérez, the current president of Real Madrid, announced that if he wins the upcoming elections this Sunday, he will confirm the signing of Dutch player Denzel Dumfries, currently with Inter Milan. The 1.88-meter-tall player is known for his aerial ability, speed in marking, and overlapping runs on the right flank. The 'Merengue' club is prepared to pay the 20 million euro release clause in his contract.",
+    fecha: "June 4, 2026",
+    tiempoLectura: "2 min"
   }
+}
 ];
