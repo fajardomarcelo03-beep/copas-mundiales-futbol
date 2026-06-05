92f9f383502de26ab29842e31e5985a1a0436e5b
fajardomarcelo03-beep <afdm721307@gmail.com>
2026-06-05 09:53:39 -0500

    Modificar estructura noticias page.js

--- src/app/[lang]/mundial-2026/noticias/[id]/page.js
+++ src/app/[lang]/mundial-2026/noticias/[id]/page.js
@@ -10,5 +10,5 @@ export async function generateMetadata({ params }) {
   };
 }
 export default function Page({ params }) {
-  return <DetalleNoticia />;
+  return <DetalleNoticia params={params}/>;
 }
