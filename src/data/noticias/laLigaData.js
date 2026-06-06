a72892524b832019df3c922be291cee0f007b331
fajardomarcelo03-beep <afdm721307@gmail.com>
2026-06-06 16:59:07 -0500

    Crear noticia-2 laLigaData

--- src/data/noticias/laLigaData.js
+++ src/data/noticias/laLigaData.js
@@ -1,5 +1,26 @@
 export const noticiasLaLiga = [
  {
+  "id": "noticia-2",
+  "categoria": "la-liga",
+  "logo": "/logo/laliga.png",
+  "fechaISO": "2026-06-06",
+  "imagen": "/LaLiga/LamineYamal.jpg",
+  "es": {
+    "titulo": "Lamine Yamal, el monarca indiscutible de La Liga",
+    "subtitulo": "La joven joya del Barcelona se alza con el premio al mejor jugador de la temporada 2025-2026.",
+    "contenido": "¡Se ha coronado el príncipe del fútbol español! Lamine Yamal ha sido galardonado oficialmente como el mejor jugador de la temporada 2025-2026 en La Liga, cerrando con broche de oro un año para la historia. A pesar de haber lidiado con complicaciones físicas que retrasaron su aparición en el inicio de la campaña, su irrupción fue volcánica: una vez sobre el césped, Yamal se convirtió en un auténtico torbellino. Con una eficacia clínica, el extremo azulgrana registró unos números de escándalo: 16 goles y 12 asistencias en apenas 28 encuentros, cifras que fueron el motor principal para que el Barcelona se adjudicara el título liguero. Su desequilibrio, visión de juego y madurez impropia de su edad lo han catapultado al trono de nuestra competición.",
+    "fecha": "6 de junio de 2026",
+    "tiempoLectura": "2 min"
+  },
+  "en": {
+    "titulo": "Lamine Yamal, the undisputed monarch of La Liga",
+    "subtitulo": "The young Barcelona star is crowned the best player of the 2025-2026 season.",
+    "contenido": "The crown of Spanish football has a new king! Lamine Yamal has been officially named the best player of the 2025-2026 La Liga season, capping off a historic year. Despite dealing with physical setbacks that delayed his start to the campaign, his emergence was volcanic: once on the pitch, Yamal became a true whirlwind. With clinical efficiency, the Barcelona winger recorded staggering numbers: 16 goals and 12 assists in just 28 matches—figures that served as the primary engine behind Barcelona’s league title success. His ability to unbalance defenses, his vision, and a maturity beyond his years have rightfully catapulted him to the throne of our competition.",
+    "fecha": "June 6, 2026",
+    "tiempoLectura": "2 min"
+  }
+},
+{
   id: "noticia-1",
   categoria: "la-liga",
   logo: "/logo/laliga.png",
