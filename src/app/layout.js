174f7b8c183324282e8323f6c19b1dff4ee963c5
fajardomarcelo03-beep <afdm721307@gmail.com>
2026-06-09 14:38:34 -0500

    Cambiar configuracion layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css';
 import HeaderContextLayout from './HeaderContextLayout';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import KlaroInitializer from '../components/KlaroInitializer';
 
 export const metadata = {
@@ -28,6 +29,7 @@ export default async function RootLayout({ children, params }) {
           {children}
         </HeaderContextLayout>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
